475163de771f5ae2c71fcd50aaa41ee527530595
shadowciaw <shadowcharmeleon@gmail.com>
2019-10-10 20:44:11 -0600

    experimenting w/ js

---
+++ scripts/main.js
@@ -0,0 +1,5 @@
+let myHeading = document.querySelector('h1');
+myHeading.textContent = 'hello world!';
+document.querySelector('html').onclick = function() {
+    alert('ouch!!! stop poking me!');
+}
